4a48a18432b5f09d003610f69937e377674422f1
ezequiel.pozzo <ezequiel.pozzo@1a37be51-7642-0410-bd2a-cbe02f7fd354>
2008-01-09 22:28:16 +0000

    First "death in floor" implementation

--- trunk/states.py
+++ trunk/states.py
@@ -1,4 +1,6 @@
 import random
+from math import tanh
+
 class Base_State():
         def __init__(self, parent_machine):
                 #raise AttributeError, "This class shouldn't be instantiated, use a children"
@@ -18,7 +20,6 @@ class Base_State():
                 pass
         
 class Wandering(Base_State):
-
         def __init__(self, parent_machine):
                 Base_State.__init__(self, parent_machine)
                 self.position=self.get_parent().rect
@@ -36,8 +37,16 @@ class Wandering(Base_State):
                         if not hasattr(self, 'landmarks'):
                                 self.landmarks=self.get_parent().standing_on.get_items()
                 actor=self.get_parent()
-                if self.target is None:
+
+                #target selection
+                while self.target is None:
                         self.target=random.choice(self.landmarks)
+                        if hasattr(self.target,'destination_gate'):
+                                deltaDeath=self.target.parent.death_toll-self.target.destination_gate.parent.death_toll
+                                decision_number=random.uniform(0,1)
+                                probability=0.5*tanh(1*deltaDeath)+0.5
+                                if probability <decision_number:
+                                        self.target=None
                         
                 target_pos=self.target.get_position()[0]
                 actor_pos=actor.get_position()[0]
@@ -71,6 +80,14 @@ class Using_Gate(Base_State):
         
         def execute(self):
                 #What should happen inside a door besides changing location?
+                #deltaDeath=self.gate.parent.death_toll-self.gate.destination_gate.parent.death_toll
+                #decision_number=random.uniform(0,1)
+                #probability=0.5*tanh(1*deltaDeath)+0.5
+                #if probability >decision_number:
+                        #self.gate.enter(self.get_parent())
+                #else:
+                        #self.gate.destination_gate.enter(self.get_parent())
+                        
                 self.gate.enter(self.get_parent())
                 self.get_parent_SM().set_state(Wandering)
                 #self.get_parent().standing_on=self.gate.destination_gate.parent
