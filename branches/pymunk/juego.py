8caffce9417e1a0a42310b6de571cc5333e14c0b
ezequiel.pozzo <ezequiel.pozzo@1a37be51-7642-0410-bd2a-cbe02f7fd354>
2008-01-10 19:11:50 +0000

    Starting to implement physics engine using chipmunk's bindings pymunk. Will compare speed when finished.

---
+++ branches/pymunk/juego.py
@@ -0,0 +1,229 @@
+import random
+from pygame.locals import *
+import pygame
+import sprites
+
+#from numpy import array, matrix
+
+SCREENRECT=Rect(0,0,800,600)
+
+random.seed(200)
+
+class Nivel():
+    bordes=Rect(0,0, 100, 100) #la unidad estandard es el metro
+    enteroAzar=random.randint
+
+    
+    
+    def __init__(self, file=None):
+        self.enemies=pygame.sprite.Group()
+        self.friends=pygame.sprite.Group()
+        self.floors=pygame.sprite.Group()
+        self.all=pygame.sprite.OrderedUpdates()
+        self.visible=pygame.sprite.OrderedUpdates()
+        sprites.Basic_Actor.set_level(self)
+
+        if file is not None:
+            self.populate_from_file(file)
+
+    def populate_from_file(self, file):
+        
+        config= eval(open(file).read())
+        self.sanity_check(config)
+        
+        floors = dict()
+        for floor_id in config['Floors']:
+            placement=list(config['Relative Placement'][floor_id])
+            placement[1]=600-placement[1]
+            floors[floor_id]=sprites.Floor(placement, round(config['Floor Sizes'][floor_id]/32.0))
+        
+        gates= dict()
+        for gate1_id in config['Gate Graph']:
+            gate2_id=config['Gate Graph'][gate1_id][0]
+            gates[gate1_id], gates[gate2_id] = sprites.Gate.create_connected()
+                
+        for floor_id in config['Hierarchy']:
+            for gate_id in config['Hierarchy'][floor_id]:
+                floors[floor_id].attach_gate(gates[gate_id], list(config['Relative Placement'][gate_id]))
+        
+        num_walkers=config['Options'].get('Walkers', 50)
+        
+        for floor in floors.values():
+            self.floors.add(floor)
+        
+        for i in range(num_walkers):
+            caminante=sprites.Caveman([self.enteroAzar(50,700),600-config['Relative Placement']['A'][1]-10], True)
+            caminante.id=i
+            self.enemies.add(caminante)
+    
+        #personaje=sprites.Volador([400,500], False)
+        #nivel_actual.friends.add(personaje)
+        
+        for floor_id in config['Initial Bodycount']:
+            floors[floor_id].death_toll=config['Initial Bodycount'].get(floor_id, 0)
+            
+        self.all.add([x.items for x in self.floors])
+        self.all.add(self.floors)
+
+        self.all.add(self.friends)
+        self.all.add(self.enemies)
+        
+        self.visible.add(self.all)
+        
+    def sanity_check(self, conf_module):
+        main_keys=['Floors', 'Gates', 'Hierarchy', 'Relative Placement', 'Gate Graph', 'Floor Sizes', 'Options']
+        for key in main_keys:
+            assert conf_module.has_key(key), 'Module key '+key+' missing'
+        Floors=conf_module['Floors']
+        Gates=conf_module['Gates']
+        Placements=conf_module['Relative Placement']
+        Graph=conf_module['Gate Graph']
+        Sizes=conf_module['Floor Sizes']
+        Hierarchy=conf_module['Hierarchy']
+        
+        for floor in Floors:
+            #checks all floors have a position
+            assert Placements.has_key(floor), 'Floor '+floor+' position not defined.'
+            assert Sizes.has_key(floor), 'Floor '+floor+' size not defined.'
+            if not Sizes[floor]%32 == 0:
+                print 'Warning, '+floor+' size is not multiple of tile size 32'
+            assert 0<=list(Placements[floor])[0]<SCREENRECT.size[0], 'Floor '+floor+' x-coordinate out of range' 
+            assert 0<=list(Placements[floor])[1]<SCREENRECT.size[1], 'Floor '+floor+' y-coordinate out of range' 
+            assert 0<=list(Placements[floor])[0]+Sizes[floor]<=SCREENRECT.size[0], 'Floor '+floor+' out of bounding box'
+            assert 0<=list(Placements[floor])[1]+32<=SCREENRECT.size[1], 'Floor '+floor+' out of bounding box'
+        
+        Parents_graph=self.invert(Hierarchy)
+        Inverse_graph=self.invert(Graph)
+        for gate in Gates:
+            assert Parents_graph.has_key(gate), 'Gate '+gate+' is an orphan :\'('
+            assert len(Parents_graph[gate])==1, 'Gate '+gate+' has more than one parent: '+str(Parents_graph[gate])
+            assert Placements.has_key(gate), 'Gate '+gate+' position not defined.'
+            assert 32<=list(Placements[gate])[0]<=round(Sizes[Parents_graph[gate][0]]/32.0)*32-32, 'Gate '+gate+' outside parent colision box' 
+            assert 0==list(Placements[gate])[1], 'Gate '+gate+' coordinate is not 0 (Must be)'
+            assert gate in Inverse_graph or gate in Graph, 'Gate '+gate+' not connected to other gate'
+            
+        for floor_id in conf_module['Initial Bodycount']:
+            assert floor_id in Floors, floor_id+' not defined in floors list'
+            
+    def invert(self, graph):
+        inv = {}
+        for k, v_list in graph.iteritems():
+            for v in v_list:
+                keys = inv.setdefault(v, [])
+                keys.append(k)
+            
+        return inv
+    
+    def set_visible(self, sprite):
+        if sprite in self.all and not sprite in self.visible:
+            self.visible.add(sprite)
+            
+    def set_invisible(self, sprite):
+        if sprite in self.all and sprite in self.visible:
+            self.visible.remove(sprite)
+            
+    def update(self, current_time):
+        self.all.update(current_time)
+        
+class Mouse(pygame.sprite.Sprite):
+    def __init__(self, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.rect=Rect([position[0], position[1], 10, 10])
+        
+def main():
+    
+    pygame.init()
+
+    screen=pygame.display.set_mode(SCREENRECT.size)
+
+    enteroAzar=random.randint
+
+    #make background
+    background = pygame.Surface(SCREENRECT.size).convert()
+    
+ 
+    
+    for i in range(1000):
+        brillo=enteroAzar(0,255)
+        background.set_at((enteroAzar(0,800),enteroAzar(0,600)), (brillo,brillo,brillo) )
+
+    #debug_rect=pygame.Rect([570,210,65,52])
+    
+    #pygame.draw.rect(background, (255,255,255), debug_rect)
+    
+    screen.blit(background, (0,0))
+    
+    pygame.display.update()
+
+    
+    #Creates level from file
+    nivel_actual=Nivel('level0.py')
+
+
+    
+
+    
+    
+    #keep track of time
+    clock = pygame.time.Clock()
+    i=0
+    #game loop
+    while 1:
+        #debug
+        i+=1
+#        if i==200:
+ #           assert False
+  #      for sprite in nivel_actual.enemigos:
+   #         if sprite.crect.colliderect(debug_rect):
+    #            bad_sprite=sprite
+     #           print 'Bad sprite found '+str(sprite.id)+' after '+str(i)+' iterations'
+      #          assert False, "Error"
+        #get input
+        for event in pygame.event.get():
+            if event.type == QUIT \
+               or (event.type == KEYDOWN and \
+                   event.key == K_ESCAPE):
+                print clock.get_fps()
+                pygame.display.quit()
+                return
+            elif event.type == KEYDOWN:
+                if event.key == K_UP:
+                    personaje.bajar_ala()
+                elif event.key == K_RIGHT:
+                    personaje.acelerar(1)
+                elif event.key == K_LEFT:
+                    personaje.acelerar(-1)
+            elif event.type == KEYUP:
+                if event.key == K_UP:
+                    personaje.subir_ala()
+                elif event.key == K_RIGHT:
+                    personaje.acelerar(0)
+            elif event.type == MOUSEBUTTONUP:
+                print 'Click at '+str(event.pos)
+                caught=pygame.sprite.spritecollide(Mouse(event.pos),nivel_actual.enemies, False)
+                if len(caught)>0:
+                    caught[0].kill()
+                    print "One dead"
+                
+        #clear sprites
+        
+        
+        #update sprites
+        nivel_actual.update(pygame.time.get_ticks())
+
+        #redraw sprites
+        rectlist=nivel_actual.visible.draw(screen)
+        pygame.display.update(rectlist)
+
+        clock.tick(50)
+        nivel_actual.visible.clear(screen, background)
+        pygame.display.set_caption("fps: " + str(clock.get_fps()))
+
+        
+    print clock.get_fps()
+    pygame.display.quit()
+        
+if __name__ == '__main__':
+    main()
+    #import profile
+    #profile.run('main()','juego.dat')
---
+++ branches/pymunk/sprites.py
@@ -0,0 +1,173 @@
+import pygame
+from math import tanh
+import random
+
+from actors import *
+from items import *
+
+SCREENRECT=Rect(0,0,800,600)
+
+from pygame.locals import *
+
+random.seed(100)
+#print "seed"
+
+#class Volador(Basic_Actor):
+    #image=None
+    #_images=[]
+    #def __init__(self, initial_position, AI):
+        #Basic_Actor.__init__(self)
+        #if Volador.image is None:
+            #Volador._images=cargar_secuencia("voladorAnim.png", 5)
+            #Volador.image = Volador._images[0][0]
+
+        #self.imagen_actual=random.randint(0,4)
+        #self.image=Volador._images[1][self.imagen_actual]
+        
+        #self.rect=self.image.get_rect()
+
+        #self.rect.center=initial_position
+        
+        
+        #self.crect=self.rect.inflate(-10,-10)
+        
+        #self.impulso_acumulado=0
+        
+        #if not AI:
+            #self.AI=False
+        #else:
+            #self.AI=True
+        
+        #self.orientacion=-1
+        #self.direccion=0
+        #self.proxima_imagen=0
+
+        #self.posicion=array(self.rect.center)
+
+        #self.aleteando=False
+        #self.estado=""
+        
+
+
+    #def update(self, current_time):
+        #self.update_phys( current_time)
+        
+        #if self.velocidad[0]*self.orientacion<0:
+            #self.orientacion=self.orientacion*-1
+            #self.image=Volador._images[(self.orientacion+1)/2][self.imagen_actual]
+
+        #if self.AI:
+            #if self.velocidad[1]>0.03:
+                #if self.estado=="bajando ala" and self.imagen_actual is 4:
+                    #self.estado="subiendo ala"
+                #elif self.estado=="subiendo ala" and self.imagen_actual is 0:
+                    #self.estado="bajando ala"
+                #elif self.estado is not "bajando ala" and self.estado is not "subiendo ala":
+                    #self.estado="bajando ala"
+            #if self.velocidad[1]<-0.03:
+                #if self.estado is not "bajando ala":
+                    #self.estado="bajando ala"
+
+
+        #if self.estado=="bajando ala" and self.imagen_actual is not 4 and self.proxima_imagen<current_time:
+            #self.imagen_actual=self.imagen_actual+1
+            #self.impulso_acumulado=self.impulso_acumulado+1
+            
+            #self.image=Volador._images[(self.orientacion+1)/2][self.imagen_actual]
+            #self.proxima_imagen=current_time+100
+            #self.aceleracion[1]=-0.0015*self.impulso_acumulado-self.velocidad[1]/200
+            #self.aceleracion[0]=self.direccion*0.002*self.impulso_acumulado
+            
+        #elif self.estado=="subiendo ala" and self.imagen_actual is not 0 and self.proxima_imagen<current_time:
+            #self.impulso_acumulado=0
+            #self.aceleracion[0]=0
+            #self.aceleracion[1]=0
+            #self.imagen_actual=self.imagen_actual-1
+            #self.image=Volador._images[(self.orientacion+1)/2][self.imagen_actual]
+            #self.proxima_imagen=current_time+100
+        #if self.imagen_actual==4:
+            #self.aceleracion[0]=0
+            #self.aceleracion[1]=-self.velocidad[1]/200
+        
+    #def bajar_ala(self):
+        #self.estado="bajando ala"
+    #def subir_ala(self):
+        #self.estado="subiendo ala"
+    #def acelerar(self, direccion):
+        #self.direccion=direccion
+
+#class Ghost(Basic_Actor):
+    #__images={}
+    #def __init__(self, original_body):
+        #Basic_Actor.__init__(self)
+        #if not original_body.__class__ in Ghost.__images:
+            #Ghost.__images[original_body.__class__]=original_body.image.copy()
+
+            
+        #self.image=Ghost.__images[original_body.__class__].copy()
+        #self.image.set_alpha(255)
+        #self.rect=original_body.rect
+        ##self.crect=pygame.Rect([0,0,0,0])
+        #self.posicion=numpy.array(original_body.rect.center)
+        #self.velocidad=numpy.array([0,-0.8])
+        #self.aceleracion=-Basic_Actor.GRAVEDAD
+        #self.prev_time=0
+        
+    #def update(self, current_time):
+        #self.update_phys(current_time)
+        #if current_time>self.prev_time:
+            #self.image.set_alpha(self.image.get_alpha()-15)
+            #if self.image.get_alpha()<=0:
+                #self.kill()
+            #self.prev_time+=200
+
+class Floor(pygame.sprite.Sprite):
+    image=None
+    _images=[]
+    total_images=8
+    def __init__(self, initial_position, ancho=2):
+        pygame.sprite.Sprite.__init__(self)
+        assert ancho>=2, 'Width too small'
+        
+        Floor._images=load_sequence("floor.png", total_images=self.total_images, load_mirror=False)[0]
+        self.image=pygame.Surface((Floor._images[0].get_width()*ancho,Floor._images[0].get_height())).convert()
+        self.image.blit(Floor._images[0], [0,0])
+        
+        for i in range(1,ancho-1):
+            self.image.blit(Floor._images[random.randint(1,self.total_images-2)], [Floor._images[0].get_width()*i,0])
+        
+        self.image.blit(Floor._images[self.total_images-1], [Floor._images[0].get_width()*(ancho-1),0] )
+        
+        self.image.set_colorkey(self.image.get_at((0,0)), RLEACCEL)
+        
+        self.rect=self.image.get_rect()
+        self.rect.topleft=initial_position
+
+        self.crect=self.rect.inflate(-32,0)
+        self.death_toll=0
+        
+        self.items=[]
+        right_cliff=Cliff(self)
+        left_cliff=Cliff(self)
+        right_cliff.set_position(self.crect.topright)
+        left_cliff.set_position(self.crect.topleft)
+        self.items.extend([right_cliff, left_cliff])
+       
+
+    def update(self, current_time):
+        pygame.sprite.Sprite.update(self)
+
+
+    def attach_gate(self, gate, position):
+        gate.parent=self
+        self.items.append(gate)
+        
+        position[0]=self.crect.topleft[0]+position[0]
+        position[1]=self.crect.topleft[1]+position[1]
+
+        gate.set_position(position)
+        
+    def get_items(self):
+        return self.items
+
+        
